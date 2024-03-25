6e2ec504f9d9ea0379bf357cb98f66b3049ac45f
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-03-25 20:43:29 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // extends: [['github:MuhammadM1998/nuxt-layers-repro']],
+  devtools: { enabled: false },
+});
